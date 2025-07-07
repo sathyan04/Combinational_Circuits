dbf85569317986cdf0681e75a6b22328ee223b62
Sathyan S S <sssathyan2004@gmail.com>
2025-07-07 12:26:04 +0530

    Update half_adder.v

--- Arithmetic_Circuits/Half Adder/half_adder.v
+++ Arithmetic_Circuits/Half Adder/half_adder.v
@@ -1,11 +1,8 @@
 `timescale 1ns / 1ps
 
-module half_adder(
-    input a,
-    input b,
-    output sum,
-    output carry
-    );
-    xor(sum,a,b);
-    and(carry,a,b);
+module half_adder(a,b,sum,carry);
+    input a, b;
+    output sum, carry;
+    xor(sum, a, b);
+    and(carry, a, b);
 endmodule
