8800c3423141a7d122bed47656829a9b8aaa3f3a
Sathyan S S <sssathyan2004@gmail.com>
2025-07-07 12:12:26 +0530

    Delete Full Subtractor directory

--- Full Subtractor/full_subtractor.v
+++
@@ -1,9 +0,0 @@
-`timescale 1ns / 1ps
-
-module full_subtractor(a,b,c,diff,bor);
-    input a,b,c;
-    output diff,bor;
-    assign diff = a^b^c;
-    assign d=~a;
-    assign bor=(d&b)|(b&c)|(d&c); 
-endmodule
