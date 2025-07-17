34975f7c8180070b3d4e2dbbf2145e49399ec852
Sathyan S S <sssathyan2004@gmail.com>
2025-07-17 12:14:49 +0530

    Update mux_2x1.v

--- Data_Routing_Circuits/Multiplexers/Mux 2x1/mux_2x1.v
+++ Data_Routing_Circuits/Multiplexers/Mux 2x1/mux_2x1.v
@@ -2,6 +2,12 @@
 
 module mux_2x1(a,b,sel,y);
     input a,b,sel;
-    output y;
-    assign y = (sel)?b:a;
+    output reg y;
+    always@(*)begin
+        case(sel)
+            0:y=a;
+            1:y=b;
+            default: y=1'bx;
+        endcase
+    end
 endmodule
